3903901e4fd333ead44f26ce82f3ccb08c2e695f
Kristinita <SashaChernykhEmpressOfUniverse@kristinita.ru>
2016-09-30 14:40:44 +0300

    Add syntax for DrCalendar

--- drCalendar.py
+++ drCalendar.py
@@ -29,13 +29,13 @@ class TDrCalend():
         s = '{0: %B %Y}'.format(self.dd)
         s = '{0: ^21}'.format(s)+"\n"
         # s += '\n'
-        
+
         for w in mc:
             for d in w:
             	s += ' ' + '{0:%a}'.format(d)[0:2]
             s += '\n'
             break
-        
+
         for w in mc:
             for d in w:
                 if (d.month == self.m):
@@ -53,8 +53,8 @@ class DrMonthsCalendarOpen(sublime_plugin.TextCommand):
 	def is_visible(self):
 		return True
 	def is_enabled(self):
-		return True		
-	def run(self, edit):	
+		return True
+	def run(self, edit):
 		loc = locale.getlocale() # get current locale
 		if sys.version[0] == "2":
 			locale.setlocale(locale.LC_ALL, 'C')
@@ -74,52 +74,52 @@ class DrMonthsCalendarOpen(sublime_plugin.TextCommand):
 		c4 = []
 
 		for x in range(0,8):
-			if len(c11) <= x: 
+			if len(c11) <= x:
 				c4.append('{0: >23}'.format(""))
 			else:
 				c4.append('{0: >23}'.format(c11[x]))   # use '*' as a fill char
-			if len(c22) <= x: 
+			if len(c22) <= x:
 				c4[x] = c4[x] + '  ' +'{0: <21}'.format("")
 			else:
 				c4[x] = c4[x] + '  ' +'{0: <21}'.format(c22[x])
-			if len(c33) <= x:  
+			if len(c33) <= x:
 				c4[x] = c4[x] + '  ' +'{0: <21}'.format("")
 			else:
 				c4[x] = c4[x] + '  ' +'{0: <21}'.format(c33[x])
 
 		# for pos in self.view.sel():
 		# 	b = pos.begin()
-		# 	rr = self.view.insert(edit, pos.begin(), "\n".join(c4)+'\n')			
+		# 	rr = self.view.insert(edit, pos.begin(), "\n".join(c4)+'\n')
 		# 	r = sublime.Region(b, b+rr)
 		# 	print ("\n".join(c4)+'\n')
 		# 	self.view.add_regions('DrCalendar',[r],"keyword",'',sublime.HIDDEN)
-			
+
 		self.output_view = self.view.window().get_output_panel("textarea_drcalendar")
 		self.view.window().run_command("show_panel", {"panel": "output.textarea_drcalendar"})
-		# self.output_view.run_command("set_file_type", {"Packages/DrCalendar/DrCalendar.tmLanguage"})
-		self.output_view.set_syntax_file("Packages/DrMonthsCalendar/DrCalendar.tmLanguage")
+		# self.output_view.run_command("set_file_type", {"Packages/DrCalendar/DrCalendar.sublime-syntax"})
+		self.output_view.set_syntax_file("Packages/DrMonthsCalendar/DrCalendar.sublime-syntax")
 		self.output_view.set_read_only(False)
 
 		# self.output_view.insert(edit, self.output_view.size(), today.strftime('   %A %x')+"\n\n")
 		self.output_view.insert(edit, self.output_view.size(),"\n".join(c4))
 		self.output_view.insert(edit, self.output_view.size(), "\n"+today.strftime('   %A %x'))
-		
+
 		locale.setlocale(locale.LC_ALL, loc)
 
-		
+
 		# self.output_view.insert(edit, self.output_view.size(),"\n".join(c22)+'\n')
-		
+
 		# self.output_view.run_command("append", {"characters": "Hello, World!"})
 		# self.output_view.set_read_only(True)
 		# r = sublime.Region(1, 100)
 		# # DRAW_OUTLINED
 		# self.output_view.add_regions('DrCalendar',[r],"keyword",'',sublime.HIDDEN)
 		# self.view.window().focusView(self.output_view)
-		
+
 		# self.view.window().show_quick_panel("123", None, sublime.MONOSPACE_FONT)
 
 
-  		
+
 
 class DrCalendarInsertMonthCommand1(sublime_plugin.TextCommand):
 	def run(self, edit):
